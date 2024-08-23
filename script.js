27f0959a0ee5495faddeee1e8bec25f33fdec05d
Ramana-ks <ramanasugumar2003@gmail.com>
2024-08-23 05:47:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var addpopupbutton=document.getElementById("addpopup-button")
+
+
+addpopupbutton.addEventListener("click",function(){
+
+    popupoverlay.style.display="block"
+    popupbox.style.display="block"
+
+})
